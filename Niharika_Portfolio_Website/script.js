769d2dcbec595c8c71ff3696eceb4f355ff75e16
NiharikaKomaragiri <niharikakomaragiri@gmail.com>
2026-09-04 11:32:43 +0530

    Delete Niharika_Portfolio_Website directory

--- Niharika_Portfolio_Website/script.js
+++
@@ -1,4 +0,0 @@
-const btn=document.getElementById('menuBtn');
-const nav=document.getElementById('nav');
-btn.addEventListener('click',()=>nav.classList.toggle('open'));
-document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
